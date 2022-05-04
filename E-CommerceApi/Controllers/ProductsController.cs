69fb7ccfb6c7e2b42eb2fb57623bd1e3ae93b9c9
ankitbarela <ankitvish1306@gmail.com>
2022-05-04 16:54:39 +0530

    Added the product data

--- E-CommerceApi/Controllers/ProductsController.cs
+++ E-CommerceApi/Controllers/ProductsController.cs
@@ -11,7 +11,11 @@ namespace E_CommerceApi.Controllers
     [Route("[controller]")]
     public class ProductsController : ControllerBase
     {
-       
+        //readonly Products[] Product;
+        ////{
+        ////     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        //// };
+
         private readonly ILogger<ProductsController> _logger;
 
         public ProductsController(ILogger<ProductsController> logger)
@@ -22,18 +26,29 @@ namespace E_CommerceApi.Controllers
         [HttpGet]
         public IEnumerable<Products> Get()
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 1).Select(index => new Products
+           var  prod = new List<Products>();
+
+            Products[] Product = new Products[2];
+            Product[0] = new Products { 
+            Id = 1,
+            Name = "Ore Sweater",
+            Size = "M, XL, XXL",
+            Color = "Custom made to what you want",
+            Price = "15000",
+            Public_id = "store / store4_rlkr3u",
+            QuantityInStock = 20
+            };
+            Product[1] = new Products
             {
-                Id = 1,
-                Name = "Ore Sweater",
-                Size = "M, XL, XXL",
-                Color = "Custom made to what you want",
-                Price = "15000",
-                Public_id = "store/store4_rlkr3u",
-                QuantityInStock = 20
-            });
-            
+                Id = 2,
+                Name = "McPherson T-shirt",
+                Size = "S, M, L",
+                Color = "White",
+                Price = "4000",
+                Public_id = "store/store2_iuj9gd",
+                QuantityInStock = 13
+            };
+            return Product;
         }
     }
 }

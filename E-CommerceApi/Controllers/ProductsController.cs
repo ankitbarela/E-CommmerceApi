ee2741843f7511a2234795710b85044d08eac5b7
ankitbarela <ankitvish1306@gmail.com>
2022-05-04 11:28:35 +0530

    Added the product conteroller

---
+++ E-CommerceApi/Controllers/ProductsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace E_CommerceApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductsController : ControllerBase
+    {
+       
+        private readonly ILogger<ProductsController> _logger;
+
+        public ProductsController(ILogger<ProductsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Products> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 1).Select(index => new Products
+            {
+                Id = 1,
+                Name = "Ore Sweater",
+                Size = "M, XL, XXL",
+                Color = "Custom made to what you want",
+                Price = "15000",
+                Public_id = "store/store4_rlkr3u",
+                QuantityInStock = 20
+            });
+            
+        }
+    }
+}
---
+++ E-CommerceApi/Products.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace E_CommerceApi
+{
+    public class Products
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public string Size { get; set; }
+        public string Color { get; set; }
+        public string Price { get; set; }
+        public string Public_id { get; set; }
+        public int QuantityInStock { get; set; }
+
+    }
+}
